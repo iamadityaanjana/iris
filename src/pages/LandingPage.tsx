c45209bd7903dd5c3f72e848e4938200bf4c1621
iamadityaanjana <iamadityaanjana@gmail.com>
2025-01-19 21:51:29 +0530

    changes the way data is fetched

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Twitter, AlertCircle } from 'lucide-react';
-import {getFlowchart} from '../../api/flow.js';
+
 
 function LandingPage() {
   const [topic, setTopic] = useState('');
